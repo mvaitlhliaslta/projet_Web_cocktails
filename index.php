4668485fc6441cee7cf70317f48d0d176d2cec7c
Hoff Eliot <eliot.hoff7@gmail.com>
2021-11-29 15:36:29 +0100

    nouveau index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Gestion de cocktails</title>
+</head>
+
+<body>
+
+<nav>
+	<?php
+		include("nav.php");
+	?>
+</nav>
+
+</body>
+
+</html>
